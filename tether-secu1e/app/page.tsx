75c132f8ea8e78297407521c64aa2d5c17cff6e2
tethersecure-lgtm <tethersecure@gmail.com>
2025-08-31 15:57:07 +0300

    Add files via upload

---
+++ tether-secu1e/app/page.tsx
@@ -0,0 +1,2 @@
+import TetherWebsite from "../components/TetherWebsite";
+export default function Page(){ return <TetherWebsite/> }
---
+++ tether-secu1e/components/TetherWebsite.tsx
@@ -0,0 +1,4 @@
+// Buraya daha önce canvas'ta oluşturulan React kodunu yapıştırabilirsiniz.
+export default function TetherWebsite(){
+  return <div className='p-10'>TETHER Secure Website – Demo</div>;
+}
